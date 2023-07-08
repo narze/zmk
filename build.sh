8c93b857bc3edbafeebcef6ebf51113d813b4578
Manassarn Manoonchai <manassarn@gmail.com>
2023-07-08 11:39:16 +0000

    Add echo

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ do
 	west build -b nice_nano_v2 -s /workspaces/zmk/app -d /workspaces/zmk-config/build/aurora_lily58_$side.build  -- -DSHIELD="splitkb_aurora_lily58_$side nice_view_adapter nice_view" -DZMK_CONFIG="/workspaces/zmk-config/config"
 	cp /workspaces/zmk-config/build/aurora_lily58_$side.build/zephyr/zmk.uf2 /workspaces/zmk-config/build/aurora_lily58_$side.uf2
 done
+
+echo "Both sides are built for Aurora Lily58"
